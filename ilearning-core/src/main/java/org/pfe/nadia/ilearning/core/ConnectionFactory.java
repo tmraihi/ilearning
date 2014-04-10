55c27349d1f95e650dde6bc2708b15f75888bffa
TMR <tmraihi@mediametrie.fr>
2014-04-10 09:41:47 +0200

    init

---
+++ ilearning-core/src/main/java/org/pfe/nadia/ilearning/core/ConnectionFactory.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pfe.nadia.ilearning.core;
+
+/**
+ *
+ * @author samsung
+ */
+public class ConnectionFactory {
+    
+}
